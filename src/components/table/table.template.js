dc29179199beaf8683cc49611a5ce85515ae724c
Egor <evn88fx64@gmail.com>
2020-07-11 22:29:08 +0400

    add generate rows

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -5,9 +5,9 @@ const CODES = {
   Z: 90
 }
 
-function createCell() {
+function toCell() {
   return `
-  <div class="cell" contenteditable>B2</div>
+  <div class="cell" contenteditable></div>
   `
 }
 
@@ -15,10 +15,10 @@ function toColumn(col) {
   return `<div class="column">${col}</div>`
 }
 
-function createRow(content) {
+function createRow(index, content) {
   return `
   <div class="row">
-    <div class="row-info"></div>
+    <div class="row-info">${index ? index : ''}</div>
     <div class="row-data">${content}</div>
   </div>
   `
@@ -28,7 +28,7 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable(rowsCount = 15) {
+export function createTable(rowsCount = 50) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
 
@@ -38,11 +38,15 @@ export function createTable(rowsCount = 15) {
       .map(toColumn)
       .join('')
 
-
-  rows.push(createRow(cols))
+  rows.push(createRow(null, cols))
 
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow())
+    const cells = new Array(colsCount)
+      .fill('')
+      .map(toCell)
+      .join('')
+
+    rows.push(createRow(i+1, cells))
   }
 
   return rows.join('')
