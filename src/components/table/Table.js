12eca628464eace2e1fef5d26ace86ce25d4c6ad
Egor <evn88fx64@gmail.com>
2020-07-12 13:25:00 +0400

    04_02

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,32 @@
-import {ExcelComponent} from '@core/ExcelComponent'
-import {createTable} from '@/components/table/table.template'
+import { ExcelComponent } from '@core/ExcelComponent'
+import { createTable } from '@/components/table/table.template'
 
 export class Table extends ExcelComponent {
-    static className = 'excel__table'
-    toHTML() {
-      return createTable()
-    }
+  static className = 'excel__table'
+
+  constructor($root) {
+    super($root, {
+      listeners: ['click', 'mousedown', 'mousemove', 'mousedown', 'mouseup']
+    })
+  }
+
+  toHTML() {
+    return createTable()
+  }
+
+  onClick() {
+    console.log('click')
+  }
+
+  onMousedown(event) {
+    console.log('mousedown', event.target)
+  }
+
+  onMousemove() {
+    console.log('mousemove')
+  }
+
+  onMouseup() {
+    console.log('mouseup')
+  }
 }
